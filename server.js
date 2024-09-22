3c817db0edbc8917808b0df0a682df083831be09
Ayush Singh Rathore <singhrathoreayush824@gmail.com>
2024-09-22 17:42:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // Allow cross-origin requests from your frontend's origin
 app.use(cors({
-  origin: 'https://bajaj-finserv-5u8b.vercel.app/Backend' // Replace with the origin of your frontend
+  origin: 'https://bajaj-finserv-5u8b.vercel.app' // Replace with the origin of your frontend
 }));
 
 // Middleware to parse JSON data
